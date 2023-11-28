f36a683795ac1a8b12edaf00a4a026b11894db9a
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-11-28 12:42:49 +0100

    play with bean configuration

--- src/main/java/elodiedaumaljava/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/elodiedaumaljava/learnspringframework/App02HelloWorldSpring.java
@@ -10,6 +10,9 @@ public class App02HelloWorldSpring {
   System.out.println("My name is " + context.getBean("name"));
   System.out.println("I am " + context.getBean("age"));
   System.out.println(context.getBean("person"));
-  System.out.println(context.getBean("adress"));
+  System.out.println(context.getBean("person2"));
+  System.out.println(context.getBean("person3"));
+  System.out.println(context.getBean("adress2"));
+  System.out.println(context.getBean(Adress.class));
  }
 }
--- src/main/java/elodiedaumaljava/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/elodiedaumaljava/learnspringframework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package elodiedaumaljava.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) {
+record Person(String name, int age, Adress adress) {
 }
 
 record Adress(String street, int number, int postcode, String city) {
@@ -23,12 +23,34 @@ public class HelloWorldConfiguration {
  }
 
  @Bean
+ public String name2() {
+  return "Luffy";
+ }
+
+ @Bean
+ public int age2() {
+  return 20;
+ }
+
+ @Bean
  public Person person() {
-  var person = new Person("Elisa", 37);
+  var person = new Person("Elisa", 37, new Adress("street", 007, 29000, "the best"));
   return person;
  }
 
  @Bean
+ public Person person2() {
+  var person = new Person(name(), age(), adress());
+  return person;
+ }
+
+ @Bean
+ public Person person3(String name, int age, Adress adress2) {
+  var person = new Person(name, age, adress2);
+  return person;
+ }
+
+ @Bean(name = "adress2")
  public Adress adress() {
 
   return new Adress("Calle Gardenias", 19, 29013, "Malaga");
