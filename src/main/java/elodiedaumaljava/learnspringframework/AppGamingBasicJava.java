710f0d0af1d414dd4ecca34d5e6b3f43fa0c22c3
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-11-28 11:47:00 +0100

    exercice: create 3rd game

--- src/main/java/elodiedaumaljava/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/elodiedaumaljava/learnspringframework/AppGamingBasicJava.java
@@ -3,11 +3,13 @@ package elodiedaumaljava.learnspringframework;
 import elodiedaumaljava.learnspringframework.game.GameRunner;
 import elodiedaumaljava.learnspringframework.game.MarioGame;
 import elodiedaumaljava.learnspringframework.game.Pacman;
+import elodiedaumaljava.learnspringframework.game.StreetFighter;
 
 public class AppGamingBasicJava {
  public static void main(String[] args) {
 
-  var game = new Pacman();
+  var game = new StreetFighter();
+  // var game = new Pacman();
   // var game = new MarioGame();
   var gameRunner = new GameRunner(game);
   gameRunner.run();
---
+++ src/main/java/elodiedaumaljava/learnspringframework/game/StreetFighter.java
@@ -0,0 +1,20 @@
+package elodiedaumaljava.learnspringframework.game;
+
+public class StreetFighter implements GamingConsole {
+
+ public void up() {
+  System.out.println("Move up");
+ }
+
+ public void down() {
+  System.out.println("Move down");
+ }
+
+ public void left() {
+  System.out.println("Shoot");
+ }
+
+ public void right() {
+  System.out.println("Kick");
+ }
+}
