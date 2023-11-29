3bf84ee644564492b0088de55d6273e5218be232
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-11-29 13:14:33 +0100

    differnece between normal bean and prototype bean

--- src/main/java/elodiedaumaljava/learnspringframework02/examples/a0/DepInjectionLauncherApp.java
+++ src/main/java/elodiedaumaljava/learnspringframework02/examples/a0/DepInjectionLauncherApp.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("elodiedaumaljava.learnspringframework02.examples.a0")
+@ComponentScan
 public class DepInjectionLauncherApp {
 
  public static void main(String[] args) {
